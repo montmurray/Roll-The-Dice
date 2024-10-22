07f4faae7ea940b1a5e223d7238384609706632f
Tessa Murray <tmurray@bsd220.org>
2024-10-22 07:48:19 -0700

    Created random die values.

--- Roll The Dice/ContentView.swift
+++ Roll The Dice/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var randomValue = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Dice Roll")
+                .font(.title)
+            Text("\(randomValue)")
+                .font(.system(size:72))
+                .padding()
+                .onTapGesture {
+                    randomValue = Int.random(in: 1...6)
+                }
+            Spacer()
         }
-        .padding()
+        
     }
 }
 
